c73b858e68068a79606389a38e0fad328bde91b0
Imtua <itsololevelling@gmail.com>
2023-06-20 10:12:49 +0300

    Добавьте файлы проекта.

---
+++ MyFitnessHomework.BL/Model/Gender.cs
@@ -0,0 +1,32 @@
+﻿namespace FitnessHomeApplication.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Метод конструктор. Создать новый пол.
+        /// </summary>
+        /// <param name="name"> Имя пола. </param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Название пола не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
